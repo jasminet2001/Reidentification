9287c11471c6a8a20cf9a3ce44e6a30f42008ae2
Jasmine <jasminesr2001@gmail.com>
2023-01-05 10:46:57 +0330

    background

--- app/src/components/feature-2/vidCard.js
+++ app/src/components/feature-2/vidCard.js
@@ -1,10 +1,11 @@
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import vid from '../../assets/Pine.mp4';
+import styles from './cards.module.css';
 
 function vidCard(){
     return (
-      <Card style={{width:'70%' ,margin:'auto', marginTop:'0%', marginBottom:'4%', backgroundColor: 'rgba(254,253,112,1)'}}>
+      <Card className={styles.bg} style={{width:'70%' ,margin:'auto', marginTop:'0%', marginBottom:'4%', backgroundColor: 'rgba(254,253,112,1)'}}>
         <Card.Body>
           <Card.Text>
             <video controls width='100%' poster=''>
