3c57e2a2feb31adf872b0dfc1969d97841775874
Jasmine <jasminesr2001@gmail.com>
2023-01-07 21:49:30 +0330

    added router and created upload component

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import VideoComponent from './components/feature-2/VideoComponent';
+import UploadComponent from './components/feature-2/UploadComponent';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
 
   return (
-    <div className="App">
-      <VideoComponent></VideoComponent>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/redirect-video">
+            <VideoComponent></VideoComponent>
+          </Route>
+          <Route path="/upload-file">
+            <UploadComponent></UploadComponent>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ app/src/components/feature-2/UploadComponent.js
@@ -0,0 +1,23 @@
+import Form from 'react-bootstrap/Form';
+import Pic from '../../assets/upload.svg'
+import styles from './upload.module.css';
+
+function UploadComponent(){
+    return(
+
+        <div className={styles.flexContainer}>
+            <div className={styles.img}>
+                <img src={Pic} className={styles.pic} alt='' />
+            </div>
+
+            <div className={styles.form}>
+                <Form.Group controlId="formFileLg" className="mb-3">
+                    <Form.Control type="file" size="lg" />
+                </Form.Group>
+            </div>
+            
+
+        </div>
+    );
+};
+export default UploadComponent;
