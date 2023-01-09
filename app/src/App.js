eec26c2821931998f6bd240f65464e45701f74ca
Jasmine <jasminesr2001@gmail.com>
2023-01-09 20:01:14 +0330

    change feature-1 background

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import VideoComponent from './components/feature-2/VideoComponent';
 import UploadComponent from './components/feature-2/UploadComponent';
+import DoubleVideo from './components/Feature-1/First';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
             <UploadComponent></UploadComponent>
           </Route>
           <Route path="/video">
-
+            <DoubleVideo></DoubleVideo>
           </Route>
         </Switch>
       </div>
